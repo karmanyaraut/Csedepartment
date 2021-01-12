9bea937657dcf59618a078f35bab758235a8cf65
karmanyaraut <karmanyaraut@gmail.com>
2021-01-12 23:05:58 +0530

    allowed host on this

--- nitkcs/nitkcs/settings.py
+++ nitkcs/nitkcs/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '&wgc0=9sl+hpw+ja*qnr71fp%wp3xvrj_h$eua*)x8#-x3e#j0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['karmanyaraut.pythonanywhere.com']
 
 
 # Application definition
